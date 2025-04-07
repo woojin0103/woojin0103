736cd09323bb358829836cefb61220470bafc358
woojin0103 <ahawow0103@naver.com>
2025-04-07 15:49:18 +0900

    장르추천 완성

--- front/movie_js.js
+++ front/movie_js.js
@@ -2,6 +2,7 @@ let MovieObject = {
     init: function(){
         
     },
+    
     getall: function(){
         
         $.ajax({
@@ -13,9 +14,50 @@ let MovieObject = {
             console.log(response)
             movielist = response.result
 
-            topdiv = document.createElement("div")
-            topdiv.style = "column-count:5"
-            document.body.appendChild(topdiv)
+            // topdiv = document.createElement("div")
+            // topdiv.style = "column-count:5"
+            // document.body.appendChild(topdiv)
+            topdiv = document.getElementById("alldiv")
+
+            movielist.forEach(movie => {
+                cmovie = document.createElement("div")
+                cmovie.className = "card"
+
+                mimg = document.createElement("img")
+                mimg.className = "card-img-top"
+                mimg.src = movie.poster_path
+                mimg.style.cursor = "pointer"
+                mimg.onclick = function(){
+                    // location.href = movie.url //
+                    window.open(movie.url)
+                }
+                cmovie.appendChild(mimg)
+                topdiv.appendChild(cmovie)
+            });
+            
+
+        }).fail(function(error){
+        //실패코드
+            console.log(error)
+        });
+    },
+
+    getgenres: function(){
+        
+        $.ajax({
+        // 실행할 코드
+            type: "GET",
+            url: `http://localhost:8000/genresbest/${document.getElementById("sgenre").value}`,
+        }).done(function(response){
+        // 성공코드
+            console.log(response)
+            movielist = response.result
+
+            topdiv = document.getElementById("genrediv")
+            topdiv.innerHTML = "" // 기존영화를 지우는 첫번째 방법
+            // while(topdiv.firstChild){
+            //     topdiv.removeChild
+            // }
 
             movielist.forEach(movie => {
                 cmovie = document.createElement("div")
@@ -43,3 +85,4 @@ let MovieObject = {
 
 MovieObject.init();
 MovieObject.getall();
+MovieObject.getgenres();
